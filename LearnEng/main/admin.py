64f3dab0766134937841aee4de1be2508adb2eda
Shivansh Tiwari <shivansh.jmis@gmail.com>
2025-10-30 15:20:19 +0530

    Debugged

--- LearnEng/main/admin.py
+++ LearnEng/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Lesson
 
+
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('title', 'uploaded_by', 'created_at')
+    list_display = ("title", "created_at")
